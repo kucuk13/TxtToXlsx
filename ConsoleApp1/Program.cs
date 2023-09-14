b1003dc97bb8df34f9dd4fd3ed105754a3239219
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2023-09-14 18:48:03 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using OfficeOpenXml;
+
+class Program
+{
+    static string fileName = "test";
+
+    static void Main()
+    {
+        ExcelPackage.LicenseContext = LicenseContext.Commercial;
+
+        string[] lines = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\" + fileName + ".txt");
+
+        using (var package = new ExcelPackage())
+        {
+            var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] values = lines[i].Split(';');
+                for (int j = 0; j < values.Length; j++)
+                {
+                    worksheet.Cells[i + 1, j + 1].Value = values[j];
+                }
+            }
+
+            package.SaveAs(new FileInfo(Directory.GetCurrentDirectory() + "\\" + fileName + ".xlsx"));
+        }
+    }
+}
